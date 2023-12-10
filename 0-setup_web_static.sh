a01b6dfedc02681910db41db3502bdaef986fa80
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-12-10 13:51:28 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,30 @@
 #!/usr/bin/env bash
+# install nginx and make folders
 
 # Install Nginx if not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    apt-get -y update
+    apt-get -y install nginx
+fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
+# Create necessary folders
+folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
+for folder in "${folders[@]}"; do
+    mkdir -p "$folder"
+done
 
-# Create a fake HTML file for testing
-echo "<html><head></head><body>Testing Nginx configuration with web_static</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# Create a fake HTML file
+echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate symbolic link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of /data/ folder to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+# Give ownership to the ubuntu user and group
+chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-config_content="
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+config_block="location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
+sed -i "/server_name _;/a $config_block" /etc/nginx/sites-available/default
 
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-        index index.html;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-"
-
-echo "$config_content" | sudo tee /etc/nginx/sites-available/default > /dev/null
-
-# Restart Nginx to apply changes
-sudo systemctl restart nginx
+# Restart Nginx
+sudo service nginx restart
