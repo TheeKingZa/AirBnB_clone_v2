fe01cb7ca80a79255aae92e7a30f09fc05de762c
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-12-10 14:17:06 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # install nginx and make folders
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root or with sudo."
+    exit 1
+fi
 
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
+    sudo apt-get -y update && sudo apt-get -y install nginx
 fi
 
 # Create necessary folders
 folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
 for folder in "${folders[@]}"; do
-    mkdir -p "$folder"
+    sudo mkdir -p "$folder" && echo "Created $folder"
 done
 
 # Create a fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create or recreate symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R $USER:$USER /data/
 
 # Update Nginx configuration
 config_block="location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
-sed -i "/server_name _;/a $config_block" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a $config_block" /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
