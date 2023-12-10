81be5fb164d583f2712660f36bd663250527e05c
Pule <proceedingmc@gmail.com>
2023-12-10 22:47:44 +0200

    pycodestyle fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-Fabric script to generate a .tgz archive from the contents of the web_static folder.
+Fabric script to generate a .tgz archive
+from the contents of the web_static folder.
 """
 
 from fabric.api import local
 from datetime import datetime
 import os
+
 
 def do_pack():
     """
