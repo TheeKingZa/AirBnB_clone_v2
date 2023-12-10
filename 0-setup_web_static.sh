a372b8f90d9497e6ba529e8af1cdf4e7b4ea4031
Pule <proceedingmc@gmail.com>
2023-12-10 19:33:49 +0200

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,27 @@
-#!/bin/bash
-# install nginx and make folders
-
-# Check for root privileges
-# if [ "$EUID" -ne 0 ]; then
-#    echo "Please run as root or with sudo."
-#    exit 1
-# fi
+#!/usr/bin/env bash
+# This Script sets up your web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get -y update && sudo apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
 
 # Create necessary folders
-folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
-for folder in "${folders[@]}"; do
-    sudo mkdir -p "$folder" && echo "Created $folder"
-done
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo -e '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
 
 # Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Give ownership to the ubuntu user and group
-sudo chown -R $USER:$USER /data/
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-config_block="location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
-sudo sed -i "/server_name _;/a $config_block" /etc/nginx/sites-available/default
+nginx_config="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "/server_name _;/a $nginx_config" /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
+
+exit 0
