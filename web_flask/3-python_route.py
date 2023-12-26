fa9f2dc98445ce1c9456901929e107b2d5fdc024
Pule <proceedingmc@gmail.com>
2023-12-26 16:23:08 +0200

    web_flask/3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,21 +11,26 @@ def hello_hbnb():
     """Route to display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Route to display 'HBNB'"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     """Route to display 'C ', followed by the value of the text variable."""
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
-    """Route to display 'Python ', followed by the value of the text variable."""
+    """Route to display 'Python ',
+    followed by the value of the text variable."""
     return "Python {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
